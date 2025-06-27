702b29a9bc67748b764c837658d5fdd7d304aa7b
anshrightskale <adev@rightskale.com>
2025-06-26 20:44:53 -0700

    organization name as UUID for upload form

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tenant_id = db.Column(db.String(100), nullable=False)         # specify length
+    organization_name = db.Column(db.String(100), nullable=False)         # specify length
     file_name = db.Column(db.String(255), nullable=False)         # common file name max
     file_path = db.Column(db.String(512), nullable=False)         # longer for S3 paths
     uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
